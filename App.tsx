a4bb0a7f29f3f1f138e8ac85993ce5043476296b
Mauro Garcia <maurogarcia0209@gmail.com>
2022-09-06 20:18:56 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Center,
+  VStack,
+  Image,
+  SpiroKitProvider,
+  TitleOne,
+  usePoppins,
+  useSpiroKitTheme,
+  Box,
+} from "@spirokit/core";
+
+const myTheme = useSpiroKitTheme();
 
 export default function App() {
+  const fontLoaded = usePoppins();
+
+  if (!fontLoaded) return <></>;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SpiroKitProvider theme={myTheme}>
+      <Center flex={1} padding={4}>
+        <VStack alignItems={"center"} space={4}>
+          <Image
+            alt="SpiroKit logo"
+            size={"220px"}
+            resizeMode="contain"
+            source={{ uri: "https://i.imgur.com/TvHaA0H.png" }}
+          ></Image>
+          <TitleOne>Welcome to SpiroKit</TitleOne>
+        </VStack>
+      </Center>
+      <Box></Box>
+    </SpiroKitProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
